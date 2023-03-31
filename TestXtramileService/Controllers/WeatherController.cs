eb8f1fca910d8bdf01580d63af5f7a257a1b4363
bambang3 <orderananyar@gmail.com>
2023-03-31 23:32:36 +0700

    init

---
+++ TestXtramileService/Controllers/WeatherController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace TestXtramile.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+
+        // UNCOMMENT THIS IF YOU TO RUN UNIT TEST 
+
+        //private HttpClient httpClient;
+        //public WeatherController(HttpClient httpClient)
+        //{
+        //    this.httpClient = httpClient;
+        //}
+
+        public async Task<IActionResult> CallAPI(string city)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+
+                    using (HttpResponseMessage response = await client.GetAsync("data/2.5/weather?q=" + city + "&appid=96e778a0420464d6f8801bd84d8ed6a2"))
+                    {
+                        var responseContent = response.Content.ReadAsStringAsync().Result;
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var json = JToken.Parse(responseContent);
+                            var main = json.SelectToken("main");
+                            var tempFahrenheit = main.Value<double>("temp");
+                            var tempCelsius = (tempFahrenheit - 32) * 5 / 9;
+                            main["temp_celsius"] = tempCelsius;
+
+                            var formattedContent = json.ToString(Formatting.Indented);
+                            return Ok(formattedContent);
+                        }
+                        else
+                        {
+                            var formattedContent = JToken.Parse(responseContent).ToString(Formatting.Indented);
+                            return Ok(formattedContent);                           
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return Ok(ex.Message);
+                }
+            }
+        }
+
+
+
+
+        //public async Task<IActionResult> CallAPI(string city)
+        //{
+        //    using (var client = new HttpClient())
+        //    {
+        //        try
+        //        {
+        //            client.BaseAddress = new Uri("http://api.openweathermap.org");
+
+        //            using (HttpResponseMessage response = await client.GetAsync("data/2.5/weather?q=" + city + "&appid=96e778a0420464d6f8801bd84d8ed6a2"))
+        //            {
+        //                var responseContent = response.Content.ReadAsStringAsync().Result;
+
+
+        //                if (response.IsSuccessStatusCode)
+        //                {
+        //                    var formattedContent = JToken.Parse(responseContent).ToString(Formatting.Indented);
+        //                    return Ok(formattedContent);
+
+        //                }
+        //                else
+        //                {
+        //                    var formattedContent = JToken.Parse(responseContent).ToString(Formatting.Indented);
+        //                    return Ok(formattedContent);
+        //                    //return BadRequest("Some thing went wrong in the request, please check the request Uri");
+        //                }
+
+
+        //            }
+
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            return Ok(ex.Message);
+        //        }
+        //    }
+        //}
+
+    }
+
+}
---
+++ TestXtramileTests/Controllers/WeatherControllerTests.cs
@@ -0,0 +1,103 @@
+﻿
+using NUnit.Framework;
+using Moq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using TestXtramile.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Moq.Protected;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace TestXtramile.Tests
+{
+    [TestFixture]
+    public class WeatherControllerTests
+    {
+        [Test]
+        public async Task CallAPI_ReturnsOkResult_WhenApiResponseIsSuccess()
+        {
+            // Arrange
+            string city = "London";
+            string expectedContentTimezone = "3600";
+            //string expectedContent = "{ \"temperature\": 20 }";
+            var mockHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            mockHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(expectedContentTimezone)
+                });
+            var httpClient = new HttpClient(mockHandler.Object);
+            var controller = new WeatherController(httpClient);
+
+            // Act
+            var result = await controller.CallAPI(city);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var contentResult = (OkObjectResult)result;
+            var response = JsonConvert.DeserializeObject<dynamic>(contentResult.Value.ToString());
+            string timezone = response.timezone;
+
+            Assert.AreEqual(expectedContentTimezone, timezone);
+        }
+
+        [Test]
+        public async Task CallAPI_ReturnsOkResult_WhenApiResponseIsError()
+        {
+            // Arrange
+            string city = "London 123";
+            string expectedContent = "city not found";
+            var mockHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            mockHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Content = new StringContent(expectedContent)
+                });
+            var httpClient = new HttpClient(mockHandler.Object);
+            var controller = new WeatherController(httpClient);
+
+            // Act
+            var result = await controller.CallAPI(city);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var contentResult = (OkObjectResult)result;
+            var response = JsonConvert.DeserializeObject<dynamic>(contentResult.Value.ToString());
+            string message = response.message;
+
+            Assert.AreEqual(expectedContent, message);
+        }
+
+        [Test]
+        public async Task CallAPI_ReturnsOkResult_WhenApiThrowsException()
+        {
+            // Untuk mengetes ini url di client.BaseAddress = new Uri("http://api.openweathermap.org"); harus di buat salan url nya
+            string city = "London";
+            string expectedContent = "No such host is known.";
+            var mockHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            mockHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ThrowsAsync(new Exception());
+            var httpClient = new HttpClient(mockHandler.Object);
+            var controller = new WeatherController(httpClient);
+
+            // Act
+            var result = await controller.CallAPI(city);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var contentResult = (OkObjectResult)result;
+            Assert.AreEqual(expectedContent, contentResult.Value.ToString());
+        }
+
+
+
+    }
+}
